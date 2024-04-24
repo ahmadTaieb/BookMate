8deb6a3a10abfeaad492986665eb21f02440458c
Valentin Despa <valentin@example.com>
2024-04-24 20:11:09 +0300

    add

--- BookMate.DataAccess/Data/ApplicationDbContext.cs
+++ BookMate.DataAccess/Data/ApplicationDbContext.cs
@@ -1,18 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BookMate.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
-
+        
+        
     }
 }
