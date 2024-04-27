e27c4c247179310933a9b0c499a11b17bc0625ee
a7mad taieb <a7mad6aieb@gmail.com>
2024-04-27 22:37:40 +0300

    book atribute

--- BookMate.Entities/Book.cs
+++ BookMate.Entities/Book.cs
@@ -1,8 +1,34 @@
-﻿namespace book_mate.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace book_mate.Entities
 {
     public class Book
     {
+        [Key]
+        public Guid Id { get; set; }
 
-        
+        [Required]
+        public string Title { get; set; }
+
+        public string? Author  { get; set; }
+
+
+        [Required]
+        public string Categories { get; set; }
+    
+
+        public string? ImageUrl {  get; set; }
+
+        public string? Description { get; set; }
+
+        public int Number_Of_Page { get; set; }
+        public int Published_Year { get; set; }
+
+        public double Average_Rating { get; set; }
+
+        public int Ratings_Count { get; set; }
+
+
     }
 }
