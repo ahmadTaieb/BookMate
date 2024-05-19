7236bedcd7da53f2ef187244ea1b3b63a6a5388c
Valentin Despa <valentin@example.com>
2024-05-19 17:02:01 +0300

    ش

--- ServiceContracts/DTO/RegisterDTO.cs
+++ ServiceContracts/DTO/RegisterDTO.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace ServiceContracts.DTO
     public class RegisterDTO
     {
         public string Name { get; set; }
-        //[Required]
+        
+        
         public string Email { get; set; }
+        
         public string Password { get; set; }
         public string? gender { get; set; }
         public int? Age { get; set; }
